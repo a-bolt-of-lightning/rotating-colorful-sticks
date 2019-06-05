b684602130734f151af621988a80c317bc4a1d09
a_bolt_of_lightning <46372775+a-bolt-of-lightning@users.noreply.github.com>
2019-06-05 19:05:27 +0430

    added light source

--- script.js
+++ script.js
@@ -6,15 +6,21 @@ var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight
 var renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth,window.innerHeight);
 $('body').append(renderer.domElement);
-var geometry1 = new THREE.BoxGeometry(4,1,1);
-var geometry2 = new THREE.BoxGeometry(6,1,1);
+
+{
+    const color = 0xFFFFFF;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    scene.add(light);
+  }
 
 
 var  longCubesArray = [];
 
 for(i=0; i<length; i++){
-    var mat = new THREE.MeshBasicMaterial({})
-    mat.color.setHex( Math.random() * 0xffffff )
+    var mat = new THREE.MeshPhongMaterial({color:Math.random() * 0xffffff})
+    //mat.color.setHex(  )
     var geo = new THREE.BoxGeometry(2+i, 0.1, 0.1);
     var cube = new THREE.Mesh(geo, mat);
     
